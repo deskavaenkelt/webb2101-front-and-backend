860028f49105ee3330f71efa93f27501239fa642
Lars Strömberg <stromberg.lars@gmail.com>
2021-09-09 13:45:31 +0200

    Lade till CORS med inställningar

--- backend/Server.js
+++ backend/Server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())

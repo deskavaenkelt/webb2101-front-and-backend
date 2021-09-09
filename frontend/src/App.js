f37ab41f37ff03824750c215a788ef2e859046be
Lars Strömberg <stromberg.lars@gmail.com>
2021-09-09 14:08:12 +0200

    GET Alive anrop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 import { useState } from "react";
+import http from './utils/api/UsersApi'
 
 function App() {
     const [text, setText] = useState('paragraph')
+
+    function alive() {
+        http.get('/')
+            .then(function (response) {
+                console.log(response.data)
+                setText(response.data)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error)
+                return 'Error'
+            })
+            .then(function () {
+                // always executed
+            })
+    }
 
     return (
         <div>
@@ -14,7 +31,7 @@ function App() {
                 setText('New Text')
             } }>New text
             </button>
-
+            <button onClick={ alive }>alive</button>
         </div>
     );
 }

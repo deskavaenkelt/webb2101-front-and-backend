7c4198019864b62e933a615f4d1f1e80a0aceb0b
Lars Strömberg <stromberg.lars@gmail.com>
2021-09-09 11:42:32 +0200

    Refaktorerat koden

--- backend/Server.js
+++ backend/Server.js
@@ -8,89 +8,11 @@ const port = process.env.PORT
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Har finns databas relaterat
 let currentId = 14
-
-function messageUserNotFound() {
-    return {
-        status: 404,
-        text: 'User not found!'
-    }
-}
-
-function messageSuccess(message) {
-    return {
-        status: 200,
-        text: message
-    }
-}
 
 function incrementCurrentIdByOne() {
     currentId += 1
-}
-
-function createNewUser(userData) {
-    let user = {
-        id: currentId,
-        name: userData.name,
-        age: userData.age,
-        gender: userData.gender,
-    }
-    incrementCurrentIdByOne()
-    inMemoryDatabase.push(user)
-}
-
-function deleteUser(index) {
-    inMemoryDatabase.splice(index, 1)
-}
-
-function updateUser(userData) {
-    const index = getUserIndex(userData.id)
-
-    if (index === -1) {
-        return messageUserNotFound()
-    } else {
-        if (inMemoryDatabase[index].name !== userData.name) {
-            inMemoryDatabase[index].name = userData.name;
-        }
-        if (inMemoryDatabase[index].age !== userData.age) {
-            inMemoryDatabase[index].age = userData.age
-        }
-        if (inMemoryDatabase[index].gender !== userData.gender) {
-            inMemoryDatabase[index].gender = userData.gender
-        }
-
-        return messageSuccess('User updated!')
-    }
-}
-
-function getUserIndex(id) {
-    for (let i = 0; i < inMemoryDatabase.length; i++) {
-        if (inMemoryDatabase[i].id === id) {
-            return i
-        }
-    }
-    return -1
-}
-
-function getUserById(id) {
-    let index = getUserIndex(id)
-
-    if (index === -1) {
-        return messageUserNotFound()
-    } else {
-        return messageSuccess(inMemoryDatabase[index])
-    }
-}
-
-function deleteUserById(id) {
-    let index = getUserIndex(id)
-
-    if (index === -1) {
-        return messageUserNotFound()
-    } else {
-        deleteUser(index)
-        return messageSuccess('User deleted!')
-    }
 }
 
 let inMemoryDatabase = [
@@ -120,23 +42,111 @@ let inMemoryDatabase = [
     },
 ]
 
+// Svarskommunikation från API
+function messageUserNotFound() {
+    return {
+        status: 404,
+        text: 'User not found!'
+    }
+}
+
+function messageSuccess(message) {
+    return {
+        status: 200,
+        text: message
+    }
+}
+
+// Sök i databasen
+function getUserIndex(id) {
+    for (let i = 0; i < inMemoryDatabase.length; i++) {
+        if (inMemoryDatabase[i].id === id) {
+            return i
+        }
+    }
+    return -1
+}
+
+// CRUD = Create Read Update Delete
+function createNewUser(userData) {
+    let user = {
+        id: currentId,
+        name: userData.name,
+        age: userData.age,
+        gender: userData.gender,
+    }
+    incrementCurrentIdByOne()
+    inMemoryDatabase.push(user)
+}
+
+function getAllUsers() {
+    return inMemoryDatabase
+}
+
+function getUserById(id) {
+    let index = getUserIndex(id)
+
+    if (index === -1) {
+        return messageUserNotFound()
+    } else {
+        return messageSuccess(inMemoryDatabase[index])
+    }
+}
+
+function updateUser(userData) {
+    const index = getUserIndex(userData.id)
+
+    if (index === -1) {
+        return messageUserNotFound()
+    } else {
+        if (inMemoryDatabase[index].name !== userData.name) {
+            inMemoryDatabase[index].name = userData.name;
+        }
+        if (inMemoryDatabase[index].age !== userData.age) {
+            inMemoryDatabase[index].age = userData.age
+        }
+        if (inMemoryDatabase[index].gender !== userData.gender) {
+            inMemoryDatabase[index].gender = userData.gender
+        }
+
+        return messageSuccess('User updated!')
+    }
+}
+
+function deleteUser(index) {
+    inMemoryDatabase.splice(index, 1)
+}
+
+function deleteUserById(id) {
+    let index = getUserIndex(id)
+
+    if (index === -1) {
+        return messageUserNotFound()
+    } else {
+        deleteUser(index)
+        return messageSuccess('User deleted!')
+    }
+}
+
+// Kontrollerar att APIet lever
 app.get('/', function (req, res) {
     res.send('API is Alive!')
 })
 
+// API CRUD
+app.post('/users', function (req, res) {
+    createNewUser(req.body)
+    res.json('Successfully created a new user')
+})
+
 app.get('/users', function (req, res) {
-    res.json(inMemoryDatabase)
+    res.json(getAllUsers())
 })
 
 app.get('/users/:id', function (req, res) {
     const id = Number(req.params.id)
     let response = getUserById(id)
     res.status(response.status).json(response.text)
-})
-
-app.post('/users', function (req, res) {
-    createNewUser(req.body)
-    res.json('Successfully created a new user')
 })
 
 app.put('/users', function (req, res,) {
@@ -149,6 +159,7 @@ app.delete('/users/:id', function (req, res) {
     res.status(response.status).send(response.text)
 });
 
+// Startar servern
 app.listen(port, () => {
     console.log(`The server is running on port ${ port }`)
 })
